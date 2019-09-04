9babc3549b8f6d3657f820cc99b556f5e639a230
rajaneesh <polavarapu.rajaneesh@vedantu.com>
2019-09-04 20:36:12 +0530

    Added a feature to check whether a user has already signed in

--- App.js
+++ App.js
@@ -7,13 +7,24 @@ export default class App extends React.Component {
     constructor(props) {
       super(props);
 
-      this.signupUserWithEmail('randomemail@random.com', 'password');
+      //https://firebase.google.com/docs/auth/web/manage-users
+      f.auth().onAuthStateChanged(function(user) {
+        //this function returns an observer whenever there is a change in the state of the user object
+        if (user) {
+          // User is signed in.
+          console.log('User already signed in');
+        } else {
+          // No user is signed in.
+          console.log('No user is signed in at the moment');
+        }
+      });
+      //this.signupUserWithEmail('randomemail@random.com', 'password');
     }
     render() {
         console.log("Inside render");
         return (
             <View style={styles.container}>
-                <Text style = {{color : 'white'}}>In the bleak mid winter!</Text>
+                <Text style = {{ fontFamily: 'Roboto', color : 'white'}}>In the bleak mid winter!</Text>
             </View>
         );
     }
@@ -41,6 +52,5 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         // fontFamily : 'Arial',
         fontSize : 9,
-
     },
 });
