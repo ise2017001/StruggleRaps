812661afad776f207d1c32d62a3c322f4ad36e9a
rajaneesh <polavarapu.rajaneesh@vedantu.com>
2019-09-04 19:44:53 +0530

    Signup with email and password added

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {f, auth, database, storage} from './config/config';
+import {f, auth, database, storage} from './config/config.js';
 
 export default class App extends React.Component {
+
+    constructor(props) {
+      super(props);
+
+      this.signupUserWithEmail('randomemail@random.com', 'password');
+    }
     render() {
+        console.log("Inside render");
         return (
             <View style={styles.container}>
-                <Text>In the bleak mid winter!</Text>
+                <Text style = {{color : 'white'}}>In the bleak mid winter!</Text>
             </View>
         );
     }
+
+    signupUserWithEmail = (email, password) => {
+      //available in firebase docs
+      //search-term firebase user register with email and password
+      f.auth().createUserWithEmailAndPassword(email, password)
+          .then((userObj) => {
+            console.log(email, userObj);
+          }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+      });
+    }
 }
+
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#000000',
         alignItems: 'center',
         justifyContent: 'center',
+        // fontFamily : 'Arial',
+        fontSize : 9,
+
     },
 });
