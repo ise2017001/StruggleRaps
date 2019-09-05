b0154b571f1177eb488eb77f1a43efa2a4b45033
rajaneesh <polavarapu.rajaneesh@vedantu.com>
2019-09-06 02:41:03 +0530

    Login with user email and password added

--- App.js
+++ App.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import * as Facebook from 'expo-facebook';
 
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+
 import {f, auth, database, storage} from './config/config.js';
-import {SocialIcon} from 'react-native-elements';
+import {SocialIcon, Button} from 'react-native-elements';
 
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
-
+        this.state = {
+            loggedIn: false
+        };
         //https://firebase.google.com/docs/auth/web/manage-users
+        //check if the user is already logged in
+        const that = this;
         f.auth().onAuthStateChanged(function (user) {
             //this function returns an observer whenever there is a change in the state of the user object
             if (user) {
                 // User is signed in.
                 console.log('User already signed in');
-
-                    f.auth().signOut()
-                        .then(() => {
-                            console.log('User logged out');
-                        }).catch((error) => {
-                        console.log(error.code, error.message);
-                    });
-
+                that.setState({
+                    loggedIn: true
+                });
             } else {
                 // No user is signed in.
                 console.log('No user is signed in at the moment');
+                that.setState({
+                    loggedIn: false
+                })
             }
         });
         //this.signupUserWithEmail('randomemail@random.com', 'password');
@@ -37,21 +40,71 @@ export default class App extends React.Component {
         return (
             <View style={styles.container}>
 
-                <Text style={{fontFamily: 'Roboto', color: 'white'}}>In the bleak mid winter!</Text>
+                {
+                    this.state.loggedIn === true ? (
+                        <View>
+                            <Text> The user is already logged in</Text>
+                            <Button
+                                title="Logout"
+                                buttonStyle={{backgroundColor: '#e91e5e'}}
+                                onPress={() => {
+                                    this.signUserOut();
+                                }}
+                            />
+                        </View>
+                    ) : (
+                        <View>
+                            {
+                                this.state.emailLoginView === true ? (
+                                    <View>
+                                        <Text> Email : </Text>
+                                        <TextInput
+                                            onChangeText={(text) => this.setState({email: text})}
+                                            value={this.state.email}
+                                        />
 
+                                        <Text> Password : </Text>
+                                        <TextInput
+                                            onChangeText={(text) => this.setState({password: text})}
+                                            secureTextEntry={true}
+                                            value={this.state.password}
+                                        />
 
-                <SocialIcon
-                    title='Sign In With Facebook'
-                    button
-                    type='facebook'
-                    onPress={() => {
-                        this.facebookLogin();
-                    }}
-                />
+                                        <Button
+                                            title="Email Login"
+                                            buttonStyle={{backgroundColor: '#00a594'}}
+                                            onPress={() => {
+                                                this.loginUserWithEmail(this.state.email, this.state.password)
+                                                    .then(()=>{console.log("User logged in with email and password")})
+                                                    .catch((error)=>{console.log(error.code, error.message)});
+                                            }}
+                                        />
+                                    </View>
+                                ) : (
+                                    <View></View>
+                                )
+                            }
+                            <Button
+                                title="Email Login"
+                                buttonStyle={{backgroundColor: '#00a594'}}
+                                onPress={() => {
+                                    this.setState({emailLoginView: true})
+                                }}
+                            />
+                            <SocialIcon
+                                title='Sign In With Facebook'
+                                button
+                                type='facebook'
+                                onPress={() => {
+                                    this.facebookLogin();
+                                }}
+                            />
+                        </View>
+                    )
+                }
             </View>
         );
     }
-
 
 
     signupUserWithEmail = (email, password) => {
@@ -67,6 +120,31 @@ export default class App extends React.Component {
         });
     }
 
+    signUserOut = () => {
+        f.auth().signOut().then(() => {
+            console.log('User logged out');
+        }).catch((error) => {
+            console.log(error.message, error.code);
+        })
+    }
+
+    loginUserWithEmail = async (email, password) => {
+        if (email !== '' && password !== '') {
+            try {
+                let user = await f.auth().signInWithEmailAndPassword(email, password)
+                    .catch((error) => {
+                        console.log(error.code, error.message);
+                    });
+                console.log(user);
+            } catch (error) {
+                console.log(error.code, error.message);
+            }
+        } else {
+            console.log('Either the password or the email is empty!');
+        }
+    }
+
+
     async facebookLogin() {
         try {
             const {
@@ -77,7 +155,7 @@ export default class App extends React.Component {
                 declinedPermissions
             } = await Facebook.logInWithReadPermissionsAsync(
                 '486329201914935', {permissions: ['email', 'public_profile'],})
-                .then(()=>{
+                .then(() => {
                     console.log('Trying to login');
                 });
             if (type === 'success') {
@@ -91,8 +169,9 @@ export default class App extends React.Component {
         } catch ({message}) {
             console.log('Error login message', message);
         }
-        }
     }
+}
+
 
 const styles = StyleSheet.create({
     container: {
